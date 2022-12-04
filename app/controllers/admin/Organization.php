401649c2a0790d12dc030128e90c8009a65ea984
Lokman <lokman.edu@gmail.com>
2022-12-04 21:34:01 +0600

    rabiul23

--- app/controllers/admin/Organization.php
+++ app/controllers/admin/Organization.php
@@ -127,6 +127,8 @@ else
           $this->data['branch_id'] = $this->session->userdata('branch_id');
           $this->data['branch'] = $this->session->userdata('branch_id') ? $this->site->getBranchByID($this->session->userdata('branch_id')) : NULL;
       }
+
+      exit();
       
        $this->data['institutions'] = $this->organization_model->getAllInstitution();
        
@@ -157,7 +159,7 @@ else
       if($branch_id)
       {
 
-        exit();
+       
       $this->data['institution_number'] = $this->site->query("SELECT institution_type_child,  prev_institution(institution_type_child, ". $prev.", ". $branch_id.") prev_institution, SUM(increase_institution) increase,  SUM(decrease_institution) decrease FROM   ( SELECT     
       institution_type_child,  COUNT(`id`) increase_institution, 0 decrease_institution
      FROM `sma_institutionlist`
