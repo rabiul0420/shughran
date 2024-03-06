a6d17e7f991ab36ae123edeb804790747eb2fb70
tauhid <tauhid5051@gmail.com>
2024-03-06 18:38:33 +0600

    notifi

--- app/models/Site.php
+++ app/models/Site.php
@@ -442,8 +442,13 @@ class Site extends CI_Model
         return FALSE;
     }
 
-    public function getNotifications()
+
+
+    public function getNotifications_old()
     {
+
+        $branch_id = $this->session->userdata('branch_id');
+
         $date = date('Y-m-d H:i:s', time());
         $this->db->where("from_date <=", $date);
         $this->db->where("till_date >=", $date);
@@ -457,6 +462,8 @@ class Site extends CI_Model
             }
         }
         $q = $this->db->get("notifications");
+
+
         if ($q->num_rows() > 0) {
             foreach (($q->result()) as $row) {
                 $data[] = $row;
@@ -464,6 +471,52 @@ class Site extends CI_Model
             return $data;
         }
     }
+
+    public function getNotifications()
+    {
+        $branch_id = $this->session->userdata('branch_id');
+
+        $date = date('Y-m-d H:i:s', time());
+
+        $this->db->select('notifications.*');
+        $this->db->from('notifications');
+        $this->db->join('branch_notifications', 'notifications.id = branch_notifications.notification_id');
+
+
+        $this->db->where("notifications.from_date <=", $date);
+        $this->db->where("notifications.till_date >=", $date);
+
+        if (!$this->Owner) {
+            if ($this->Supplier) {
+                $this->db->where('notifications.scope', 4);
+            } elseif ($this->Customer) {
+                $this->db->where('notifications.scope', 1)->or_where('notifications.scope', 3);
+            } elseif (!$this->Customer && !$this->Supplier) {
+                $this->db->where('notifications.scope', 2)->or_where('notifications.scope', 3);
+            }
+
+            $this->db->where('branch_notifications.branch_id', $branch_id);
+        }
+
+        $q = $this->db->get();
+
+
+                $this->sma->print_arrays( $this->db->last_query());
+
+
+
+
+
+        if ($q->num_rows() > 0) {
+            foreach ($q->result() as $row) {
+                $data[] = $row;
+            }
+            return $data;
+        }
+    }
+
+
+
 
     public function getTickets()
     {
