5097dc1a9df8ed2c5bb458313c55e882c8f09ff5
Tauhid <tauhid5051@gmail.com>
2024-05-26 19:56:48 +0600

    ee

--- time.php
+++ time.php
@@ -1,13 +1,5 @@
 <?php
-echo date('Y-m-d H:i:s');
-echo 'AZ';
-?>
+$password = '123';
+$hashed_password = sha1($password);
+echo $hashed_password;
 
-01-may-24-----2H
-02-may-24 ----2H
-09-may-24 ----4H
-11-may-24 ----1H
-12-may-24-----2h
-13-may-24-----30 min
-14-may-24-----2h
-15-may-24-----
