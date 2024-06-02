279073b6d39eda4a2c219562f02242442f7b5e43
Lokman <lokman.edu@gmail.com>
2024-06-02 09:29:26 +0600

    v3_23l

--- app/controllers/admin/Others.php
+++ app/controllers/admin/Others.php
@@ -1408,14 +1408,14 @@ class Others extends MY_Controller
 		if ($branch_id) {
 
 			$this->datatables
-				->select($this->db->dbprefix('administrative_area') . ".id as areaid, date,   {$this->db->dbprefix('branches')}.name as branch_name,  v2_district_upazila(district_id) district_name,v2_district_upazila(thana_upazila_id) thana_name,v2_district_upazila(pourashava_union_id) pourashava_union_name,v2_district_upazila(ward_id) ward_name", FALSE)
+				->select($this->db->dbprefix('administrative_area') . ".id as areaid, date,   {$this->db->dbprefix('branches')}.name as branch_name,  v3_district_upazila(district_id) district_name,v3_district_upazila(thana_upazila_id) thana_name,v3_district_upazila(pourashava_union_id) pourashava_union_name,v3_district_upazila(ward_id) ward_name", FALSE)
 				->from('administrative_area');
 
 			$this->datatables->join('branches', 'branches.id=administrative_area.branch_id', 'left')
 				->where('branches.id', $branch_id);
 		} else {
 			$this->datatables
-				->select($this->db->dbprefix('administrative_area') . ".id as areaid, date,   {$this->db->dbprefix('branches')}.name as branch_name,  v2_district_upazila(district_id) district_name,v2_district_upazila(thana_upazila_id) thana_name,v2_district_upazila(pourashava_union_id) pourashava_union_name,v2_district_upazila(ward_id) ward_name", FALSE)
+				->select($this->db->dbprefix('administrative_area') . ".id as areaid, date,   {$this->db->dbprefix('branches')}.name as branch_name,  v3_district_upazila(district_id) district_name,v3_district_upazila(thana_upazila_id) thana_name,v3_district_upazila(pourashava_union_id) pourashava_union_name,v3_district_upazila(ward_id) ward_name", FALSE)
 				->from('administrative_area');
 
 			$this->datatables->join('branches', 'branches.id=administrative_area.branch_id', 'left');
