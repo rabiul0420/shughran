43d75ffad2e22508c4bfdabd24b76b0b7e31bcfb
Lokman Hossain <lokman.edu@gmail.com>
2024-12-06 18:18:36 +0600

    latest merge

--- app/helpers/excel_helper.php
+++ app/helpers/excel_helper.php
@@ -2,13 +2,13 @@
 
 if(! function_exists('create_excel')) {
     function create_excel($excel, $filename) {
-       // header("Content-Type: ".get_mime_by_extension('xlsx')." charset=utf-8");
+        header("Content-Type: ".get_mime_by_extension('xlsx')." charset=utf-8");
         header('Content-Disposition: attachment;filename="' . $filename . '.xlsx"');
         header('Cache-Control: max-age=0');
 
          
-        $objWriter = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
-        $objWriter->save('php://output');
+        //$objWriter = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
+       // $objWriter->save('php://output');
         exit;
     }
 }
