41558d19048089b9b23300db01c3848614bcb276
Lokman Hossain <lokman.edu@gmail.com>
2024-12-06 18:15:09 +0600

    latest merge

--- app/controllers/admin/Associate.php
+++ app/controllers/admin/Associate.php
@@ -2474,7 +2474,7 @@ class Associate extends MY_Controller
                
                 $this->sma->print_arrays( $data);
 
-               // create_excel($this->excel, $filename);
+               create_excel($this->excel, $filename);
             }
             $this->session->set_flashdata('error', lang('nothing_found'));
             redirect($_SERVER["HTTP_REFERER"]);
